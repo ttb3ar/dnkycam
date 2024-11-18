82eb3d7cb918cf1191b649d9a91693580d2ec20e
Teddy Lin <theobuilder@hotmail.com>
2024-11-18 00:33:33 -0800

    Created app.py
    the webapp

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)  # Use 0 for USB camera or /dev/video0 for Pi Cam
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Make sure index.html is in the 'templates' folder
+
+@app.route('/videofeed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
